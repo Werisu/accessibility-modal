44494f577dce07d2b2c17136aaeaa9102f5dd24c
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2022-06-16 17:42:09 -0300

    modal component ngTemplateOutlet

--- src/app/shared/components/modal/services/modal.service.ts
+++ src/app/shared/components/modal/services/modal.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, TemplateRef } from "@angular/core";
+import { Injectable } from "@angular/core";
+import { ModalConfig } from "../interfaces/modal-config";
 
 @Injectable()
 export class ModalService {
@@ -8,11 +9,6 @@ export class ModalService {
     return new ModalRef();
   }
 
-}
-
-export interface ModalConfig {
-  templateRef: TemplateRef<any>;
-  title: string;
 }
 
 export class ModalRef {
