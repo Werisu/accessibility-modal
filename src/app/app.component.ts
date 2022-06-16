d7e90736fba9e05bdf524f5c38fd62d3b539097b
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2022-06-16 09:03:05 -0300

    API minimalista e TemplateRef
    * Como definir uma API minimalista; * TemplateRef e seu papel; * Injeção de TemplateRef atra´ves de variáveis de template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { ModalRef, ModalService } from './shared/components/modal/services/modal.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild('modal') public modalTemplateRef!: TemplateRef<any>;
   title = 'accessibility-2';
+  public firstName = 'Wellysson';
+  public modalRef!: ModalRef;
+
+  constructor(private modalService: ModalService){}
+
+  public show(content: TemplateRef<any>){
+    this.modalRef = this.modalService.open({
+      templateRef: content,
+      title: 'User Details'
+    })
+  }
 }
