635a7357ee7acb3af4f7dd2ba14d6f0e9b2fec6f
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2022-08-30 15:42:40 -0300

    component modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,6 @@ export class AppComponent {
     this.modalRef = this.modalService.open({
       templateRef: content,
       title: 'User Details'
-    })
+    });
   }
 }
--- src/app/shared/components/modal/services/modal.service.ts
+++ src/app/shared/components/modal/services/modal.service.ts
@@ -1,20 +1,23 @@
 import { ModalComponent } from './../modal.component';
 import { ComponentFactory, ComponentFactoryResolver, ComponentRef, Injectable, Injector } from "@angular/core";
 import { ModalConfig } from "../interfaces/modal-config";
+import { BodyInjectorService } from 'src/app/shared/services/body-injector';
 
 @Injectable()
 export class ModalService {
 
   private componentFactory!: ComponentFactory<ModalComponent>;
 
-  constructor(componetFactoryResolver: ComponentFactoryResolver, private injector: Injector) {
+  constructor(componetFactoryResolver: ComponentFactoryResolver, private injector: Injector, private bodyInjector: BodyInjectorService) {
     this.componentFactory = componetFactoryResolver.resolveComponentFactory(ModalComponent);
   }
 
   public open(config: ModalConfig): ModalRef{
     const componentRef = this.createComponentRef();
     componentRef.instance.config = config;
+    console.log(componentRef.instance);
     console.log('open called');
+    this.bodyInjector.stackBeforeAppRoot(componentRef);
     return new ModalRef(componentRef);
   }
 
