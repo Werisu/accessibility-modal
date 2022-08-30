12b6614a7b7b87e0a2a9455c167e80374653dfa3
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2022-08-30 17:50:06 -0300

    angular animation

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,11 +1,14 @@
-import { Component, TemplateRef } from "@angular/core";
+import { Component, HostBinding, TemplateRef } from "@angular/core";
+import { fade } from "../../animations/fade";
 import { ModalConfig } from "./interfaces/modal-config";
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.scss'],
+  animations: [fade]
 })
 export class ModalComponent{
+  @HostBinding('@fade') fade = true;
   public config!: ModalConfig;
 }
