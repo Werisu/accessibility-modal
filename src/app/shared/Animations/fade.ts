364cbdaf3bd8c49092cb32d53a5a23d8e393c2e8
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2022-08-31 07:46:52 -0300

    event stopPropagation

--- src/app/shared/Animations/fade.ts
+++ src/app/shared/Animations/fade.ts
@@ -7,13 +7,13 @@ export const fade = trigger(
       ':enter',
       [
         style({ opacity: 0 }),
-        animate(1000, style({ opacity: 1 }))
+        animate(300, style({ opacity: 1 }))
       ]
     ),
     transition(
       ':leave',
       [
-        animate(1000, style({ opacity: 0 }))
+        animate(300, style({ opacity: 0 }))
       ]
     )
   ]
